67af17b4e8b53fe32d6790850f19ff76e098e27e
zhangheng <zhangheng19931123@gmail.com>
2017-06-06 17:53:05 +0200

    add function calculDistanceSB

--- expressionrecogniser.cpp
+++ expressionrecogniser.cpp
@@ -46,14 +46,14 @@ double ExpressionRecogniser::getSimilarity(cv::Mat_<unsigned char> img1, cv::Mat
     return this->getSimilarity(img2);
 }
 
-void ExpressionRecogniser::getAcd(int posCoor1, int posCoor2, int posCoor3){
+void ExpressionRecogniser::alignement(int posCoor1, int posCoor2, int posCoor3){
     a = (double(img1Points.at(posCoor2).getY())-double(img1Points.at(posCoor1).getY()))/(double(img2Points.at(posCoor2).getY())-double(img2Points.at(posCoor1).getY()));
     a = fabs(a);
     c = double(img1Points.at(posCoor3).getX()) - a*double(img2Points.at(posCoor3).getX());
     d = double(img1Points.at(posCoor3).getY()) - a*double(img2Points.at(posCoor3).getY());
 }
 
-void ExpressionRecogniser::drawMouth()
+void ExpressionRecogniser::calculDistanceSwainBallardMouth()
 {
     Point points1[1][12];
     for (int i = 0; i < mouthCompareNum; ++i) {
@@ -61,11 +61,11 @@ void ExpressionRecogniser::drawMouth()
     }
     const Point* pt1[1] = { points1[0] };
     int npt1[1] = {mouthCompareNum};
-    polylines( mask1, pt1, npt1, 1, 1, Scalar(255)) ;
-    fillPoly( mask1, pt1, npt1, 1, Scalar(255), 8);
+    polylines( histogrammeSwainBallard1, pt1, npt1, 1, 1, Scalar(255)) ;
+    fillPoly( histogrammeSwainBallard1, pt1, npt1, 1, Scalar(255), 8);
     if(foundface)
     {
-        getAcd(59, 65, 62);
+        alignement(59, 65, 62);
         Point points2[1][12];
         for (int i = 0; i < mouthCompareNum; ++i) {
             points2[0][i] = img2Points.at(mouthCompareOrder[i]).afterChange(a,c,d).toPoint();
@@ -73,23 +73,23 @@ void ExpressionRecogniser::drawMouth()
         }
         const Point* pt2[1] = { points2[0] };
         int npt2[1] = {mouthCompareNum};
-        polylines( mask2, pt2, npt2, 1, 1, Scalar(255)) ;
-        fillPoly( mask2, pt2, npt2, 1, Scalar(255), 8);
+        polylines( histogrammeSwainBallard2, pt2, npt2, 1, 1, Scalar(255)) ;
+        fillPoly( histogrammeSwainBallard2, pt2, npt2, 1, Scalar(255), 8);
     }
 }
 
-void ExpressionRecogniser::drawLeftEye(){
+void ExpressionRecogniser::calculDistanceSwainBallardLeftEye(){
     Point points1[1][8];
     for (int i = 0; i < leftEyeCompareNum; ++i) {
         points1[0][i] = img1Points.at(leftEyeCompareOrder[i]).toPoint();
     }
     const Point* pt1[1] = { points1[0] };
     int npt1[1] = {leftEyeCompareNum};
-    polylines( mask1, pt1, npt1, 1, 1, Scalar(255)) ;
-    fillPoly( mask1, pt1, npt1, 1, Scalar(255), 8);
+    polylines( histogrammeSwainBallard1, pt1, npt1, 1, 1, Scalar(255)) ;
+    fillPoly( histogrammeSwainBallard1, pt1, npt1, 1, Scalar(255), 8);
     if(foundface)
     {
-        getAcd(34, 30, 38);
+        alignement(34, 30, 38);
         Point points2[1][8];
         for (int i = 0; i < leftEyeCompareNum; ++i) {
             points2[0][i] = img2Points.at(leftEyeCompareOrder[i]).afterChange(a,c,d).toPoint();
@@ -97,23 +97,23 @@ void ExpressionRecogniser::drawLeftEye(){
         }
         const Point* pt2[1] = { points2[0] };
         int npt2[1] = {leftEyeCompareNum};
-        polylines( mask2, pt2, npt2, 1, 1, Scalar(255)) ;
-        fillPoly( mask2, pt2, npt2, 1, Scalar(255), 8);
+        polylines( histogrammeSwainBallard2, pt2, npt2, 1, 1, Scalar(255)) ;
+        fillPoly( histogrammeSwainBallard2, pt2, npt2, 1, Scalar(255), 8);
     }
 }
 
-void ExpressionRecogniser::drawRightEye(){
+void ExpressionRecogniser::calculDistanceSwainBallardRightEye(){
     Point points1[1][8];
     for (int i = 0; i < rightEyeCompareNum; ++i) {
         points1[0][i] = img1Points.at(rightEyeCompareOrder[i]).toPoint();
     }
     const Point* pt1[1] = { points1[0] };
     int npt1[1] = {rightEyeCompareNum};
-    polylines( mask1, pt1, npt1, 1, 1, Scalar(255)) ;
-    fillPoly( mask1, pt1, npt1, 1, Scalar(255), 8);
+    polylines( histogrammeSwainBallard1, pt1, npt1, 1, 1, Scalar(255)) ;
+    fillPoly( histogrammeSwainBallard1, pt1, npt1, 1, Scalar(255), 8);
     if(foundface)
     {
-        getAcd(40, 44, 39);
+        alignement(40, 44, 39);
         Point points2[1][8];
         for (int i = 0; i < rightEyeCompareNum; ++i) {
             points2[0][i] = img2Points.at(rightEyeCompareOrder[i]).afterChange(a,c,d).toPoint();
@@ -121,23 +121,23 @@ void ExpressionRecogniser::drawRightEye(){
         }
         const Point* pt2[1] = { points2[0] };
         int npt2[1] = {rightEyeCompareNum};
-        polylines( mask2, pt2, npt2, 1, 1, Scalar(255)) ;
-        fillPoly( mask2, pt2, npt2, 1, Scalar(255), 8);
+        polylines( histogrammeSwainBallard2, pt2, npt2, 1, 1, Scalar(255)) ;
+        fillPoly( histogrammeSwainBallard2, pt2, npt2, 1, Scalar(255), 8);
     }
 }
 
-void ExpressionRecogniser::drawLeftEyeBrow(){
+void ExpressionRecogniser::calculDistanceSwainBallardLeftEyeBrow(){
     Point points1[1][6];
     for (int i = 0; i < leftEyebrowCompareNum; ++i) {
         points1[0][i] = img1Points.at(leftEyebrowCompareOrder[i]).toPoint();
     }
     const Point* pt1[1] = { points1[0] };
     int npt1[1] = {leftEyebrowCompareNum};
-    polylines( mask1, pt1, npt1, 1, 1, Scalar(255)) ;
-    fillPoly( mask1, pt1, npt1, 1, Scalar(255), 8);
+    polylines( histogrammeSwainBallard1, pt1, npt1, 1, 1, Scalar(255)) ;
+    fillPoly( histogrammeSwainBallard1, pt1, npt1, 1, Scalar(255), 8);
     if(foundface)
     {
-        getAcd(18, 21, 21);
+        alignement(18, 21, 21);
         Point points2[1][6];
         for (int i = 0; i < leftEyebrowCompareNum; ++i) {
             points2[0][i] = img2Points.at(leftEyebrowCompareOrder[i]).afterChange(a,c,d).toPoint();
@@ -145,23 +145,23 @@ void ExpressionRecogniser::drawLeftEyeBrow(){
         }
         const Point* pt2[1] = { points2[0] };
         int npt2[1] = {leftEyebrowCompareNum};
-        polylines( mask2, pt2, npt2, 1, 1, Scalar(255)) ;
-        fillPoly( mask2, pt2, npt2, 1, Scalar(255), 8);
+        polylines( histogrammeSwainBallard2, pt2, npt2, 1, 1, Scalar(255)) ;
+        fillPoly( histogrammeSwainBallard2, pt2, npt2, 1, Scalar(255), 8);
     }
 }
 
-void ExpressionRecogniser::drawRightEyeBrow(){
+void ExpressionRecogniser::calculDistanceSwainBallardRightEyeBrow(){
     Point points1[1][6];
     for (int i = 0; i < rightEyebrowCompareNum; ++i) {
         points1[0][i] = img1Points.at(rightEyebrowCompareOrder[i]).toPoint();
     }
     const Point* pt1[1] = { points1[0] };
     int npt1[1] = {rightEyebrowCompareNum};
-    polylines( mask1, pt1, npt1, 1, 1, Scalar(255)) ;
-    fillPoly( mask1, pt1, npt1, 1, Scalar(255), 8);
+    polylines( histogrammeSwainBallard1, pt1, npt1, 1, 1, Scalar(255)) ;
+    fillPoly( histogrammeSwainBallard1, pt1, npt1, 1, Scalar(255), 8);
     if(foundface)
     {
-        getAcd(22, 25, 22);
+        alignement(22, 25, 22);
         Point points2[1][6];
         for (int i = 0; i < rightEyebrowCompareNum; ++i) {
             points2[0][i] = img2Points.at(rightEyebrowCompareOrder[i]).afterChange(a,c,d).toPoint();
@@ -169,30 +169,30 @@ void ExpressionRecogniser::drawRightEyeBrow(){
         }
         const Point* pt2[1] = { points2[0] };
         int npt2[1] = {rightEyebrowCompareNum};
-        polylines( mask2, pt2, npt2, 1, 1, Scalar(255)) ;
-        fillPoly( mask2, pt2, npt2, 1, Scalar(255), 8);
+        polylines( histogrammeSwainBallard2, pt2, npt2, 1, 1, Scalar(255)) ;
+        fillPoly( histogrammeSwainBallard2, pt2, npt2, 1, Scalar(255), 8);
     }
 }
 
 double ExpressionRecogniser::getSimilarity(cv::Mat_<unsigned char> img2)
 {
     img2Points = this->getFacePoints(img2);
-    mask1 = Mat(240, 320, CV_8UC1, Scalar(0));
-    mask2 = Mat(240, 320, CV_8UC1, Scalar(0));
-    drawMouth();
-    drawLeftEye();
-    drawRightEye();
-    drawLeftEyeBrow();
-    drawRightEyeBrow();
+    histogrammeSwainBallard1 = Mat(240, 320, CV_8UC1, Scalar(0));
+    histogrammeSwainBallard2 = Mat(240, 320, CV_8UC1, Scalar(0));
+    calculDistanceSwainBallardMouth();
+    calculDistanceSwainBallardLeftEye();
+    calculDistanceSwainBallardRightEye();
+    calculDistanceSwainBallardLeftEyeBrow();
+    calculDistanceSwainBallardRightEyeBrow();
     if(foundface){
         double temp1 = 0;
         double temp2 = 0;
-        for(int i = 0; i < mask1.rows; i++){
-            for (int j = 0; j < mask1.cols; j ++){
-                if(mask1.at<uchar>(i,j) == 255){
+        for(int i = 0; i < histogrammeSwainBallard1.rows; i++){
+            for (int j = 0; j < histogrammeSwainBallard1.cols; j ++){
+                if(histogrammeSwainBallard1.at<uchar>(i,j) == 255){
                     temp1++;
                 }
-                if(mask2.at<uchar>(i,j) == 255 && mask1.at<uchar>(i,j) == 255){
+                if(histogrammeSwainBallard2.at<uchar>(i,j) == 255 && histogrammeSwainBallard1.at<uchar>(i,j) == 255){
                     temp2++;
                 }
             }
--- expressionrecogniser.h
+++ expressionrecogniser.h
@@ -20,41 +20,35 @@ class ExpressionRecogniser
 public:
     ExpressionRecogniser(std::string dataPath);
     void setDataPath(std::string dataPath);
-    std::vector<EmoPoint> getFacePoints(cv::Mat_<unsigned char> img);
-    std::vector<EmoPoint> getAllImg2FacePoints(){return img2Points;}
+    void calculDistanceSwainBallardMouth();
+    void calculDistanceSwainBallardLeftEye();
+    void calculDistanceSwainBallardRightEye();
+    void calculDistanceSwainBallardLeftEyeBrow();
+    void calculDistanceSwainBallardRightEyeBrow();
+    void alignement(int posCoor1, int posCoor2, int posCoor3);
     double getSimilarity(cv::Mat_<unsigned char> img1, cv::Mat_<unsigned char> currentImg1);
     double getSimilarity(cv::Mat_<unsigned char> currentImg1);
     void loadImg1Points(cv::Mat_<unsigned char> img1);
 
 private:
-    Mat mask1 , mask2;
+    Mat histogrammeSwainBallard1 , histogrammeSwainBallard2;
     std::string dataPath;
     int foundface;
     std::vector<EmoPoint> img1Points;
     std::vector<EmoPoint> img2Points;
-
     const int mouthCompareOrder[12] = {59, 60, 61, 62, 63, 64, 65, 72, 73, 74, 75, 76};
     const int mouthCompareNum = 12;
-    void drawMouth();
-
     const int leftEyeCompareOrder[8] = {30, 31, 32, 33, 34, 35, 36, 37};
     const int leftEyeCompareNum = 8;
-    void drawLeftEye();
-
     const int rightEyeCompareOrder[8] = {40, 41, 42, 43, 44, 45, 46, 47};
     const int rightEyeCompareNum = 8;
-    void drawRightEye();
-
     const int leftEyebrowCompareOrder[6] = {16, 17, 18, 19, 20, 21};
     const int leftEyebrowCompareNum = 6;
-    void drawLeftEyeBrow();
-
     const int rightEyebrowCompareOrder[6] = {22, 23, 24, 25, 26, 27};
     const int rightEyebrowCompareNum = 6;
-    void drawRightEyeBrow();
-
     double a, b, c, d;
-    void getAcd(int posCoor1, int posCoor2, int posCoor3);
+    std::vector<EmoPoint> getFacePoints(cv::Mat_<unsigned char> img);
+    std::vector<EmoPoint> getAllImg2FacePoints(){return img2Points;}
 };
 
 #endif // EXPRESSIONRECOGNISER_H
